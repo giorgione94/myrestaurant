e76c3bd7165cfa1e4755e6d67f9e952be9d0453f
Giorgio Biancani <giorgio.biancani@gmail.com>
2022-12-14 10:22:49 +0100

    First commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('categories')->insert(['name' => 'Percussion']);
+        //php artisan db:seed -> Da digitare sul terminale
+    }
+}
